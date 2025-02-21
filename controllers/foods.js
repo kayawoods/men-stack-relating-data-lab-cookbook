8d8d2b03a3c9f5835eb0082eacf9b76d471fd29f
Kaya Woods <kayanevada@gmail.com>
2025-02-21 14:35:24 -0800

    community page

--- controllers/foods.js
+++ controllers/foods.js
@@ -36,7 +36,7 @@ router.post('/', async (req, res) => {
     }
   });
 
-  router.get('/:applicationId', async (req, res) => {
+  router.get('/:foodId', async (req, res) => {
     try {
       const currentUser = await User.findById(req.session.user._id);
       const food = currentUser.foods.id(req.params.foodId);
@@ -49,7 +49,59 @@ router.post('/', async (req, res) => {
     }
   });
 
+  // controllers/applications.js
 
+router.delete('/:foodId', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    currentUser.foods.id(req.params.foodId).deleteOne();
+    await currentUser.save();
+    res.redirect(`/users/${currentUser._id}/foods`);
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
+});
+
+// controllers/applications.js
+
+router.get('/:foodId/edit', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    const food = currentUser.foods.id(req.params.foodId);
+    res.render('foods/edit.ejs', {
+      food: food,
+    });
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
+});
+
+router.put('/:foodId', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    const food = currentUser.foods.id(req.params.foodId);
+    food.set(req.body);
+    await currentUser.save();
+    res.redirect(
+      `/users/${currentUser._id}/foods/${req.params.foodId}`
+    );
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
+});
+
+router.get('/', async (req, res) => {
+  try {
+      const users = await User.find({}, 'username _id'); 
+      res.render('users/index.ejs', { users });
+  } catch (error) {
+      console.log(error);
+      res.redirect('/');
+  }
+});
   
 module.exports = router;
 
--- server.js
+++ server.js
@@ -6,12 +6,15 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
+
+
 const isSignedIn = require('./middleware/is-signed-in.js');
 const passUserToView = require('./middleware/pass-user-to-view.js');
 
+
 const authController = require('./controllers/auth.js');
 const foodsController = require('./controllers/foods.js');
-
+const usersController = require('./controllers/users.js');
 
 const port = process.env.PORT ? process.env.PORT : '3000';
 
@@ -20,8 +23,6 @@ mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
-
-
 
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
@@ -33,6 +34,7 @@ app.use(
     saveUninitialized: true,
   })
 );
+
 app.use(passUserToView);
 
 app.get('/', (req, res) => {
@@ -43,10 +45,11 @@ app.get('/', (req, res) => {
 
 
 
-
 app.use('/auth', authController);
 app.use(isSignedIn);
 app.use('/users/:userId/foods', foodsController);
+app.use('/users/:userId/users', usersController);
+
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
