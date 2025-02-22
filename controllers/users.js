fa20c52defa293223b1ca502f96887808d4f7962
Kaya Woods <kayanevada@gmail.com>
2025-02-21 16:18:37 -0800

    finished lab

--- controllers/users.js
+++ controllers/users.js
@@ -1,31 +1,38 @@
 const express = require('express');
 const router = express.Router();
-
 const User = require('../models/user.js');
 
-
-//app.use('/users', usersRouter);
-
+// Get all users
 router.get('/', async (req, res) => {
     try {
-      const allUsers = await User.find();
-  
-      res.render('users/index.ejs', {
-        users: allUsers,
-      });
+        const allUsers = await User.find();
+        res.render('users/index.ejs', { users: allUsers });
     } catch (error) {
-      console.log(error);
-      res.redirect('/');
+        console.log(error);
+        res.redirect('/');
     }
-  });
+});
 
+// Get a single user and show their pantry
+router.get('/:userId', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.userId);
 
-
-
-
-
-
-
-
+        res.render('users/show.ejs', { user, foods: user.foods }); // Pass user data to show.ejs
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
+});
 
 module.exports = router;
+
+
+
+
+
+
+
+
+
+
